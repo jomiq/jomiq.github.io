bba591ba6f9d47a81ec83f91877be6b1ab80a1ab
Jon Mihkkal Inga <jon.mihkkal.inga@gmail.com>
2026-09-21 12:04:36 +0200

    demo2

--- README.md
+++
@@ -1,1 +0,0 @@
-# jomiq.github.io
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 async function getStatus() {
-  const url = "https:/jomiq.github.io/status.json";
+  const url = "/status.json";
   try {
     const response = await fetch(url);
     if (!response.ok) {
@@ -29,24 +29,7 @@ async function main() {
 
   if (status["profilesWithWebmessages"][0]["hasCriticalMessages"]) {
 
-    const text = status["profilesWithWebmessages"][0]["webMessages"][0];
-    const last_update = "1948-01-02"
-    let fulltext = ""
-    status["profilesWithWebmessages"][0]["webMessages"].forEach((t) => {
-      fulltext = fulltext + `<p>${t}</p>`
-    });
-
-    const alert_link_template = document.querySelector("#alert-link-template");
-    const alert_link_clone = document.importNode(alert_link_template.content, true);
-    alert_link_clone.id = "";
-    alert_menu_item.appendChild(alert_link_clone);
-
-    const alert_template = document.querySelector("#alert-template")
-    const alert_clone = document.importNode(alert_template.content, true);
-    alert_clone.querySelector("#messages").innerHTML = fulltext;
-
-    alert_pos.replaceWith(alert_clone);
-    footer_status.innerHTML = `${text} - ${last_update}`;
+    
   } else {
     document.removeChild(alert_pos);
     footer_status.innerHTML = status["defaultTextNoMessages"]
